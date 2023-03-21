9df045ac3d198ac720019b8de9012658636edf2c
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2023-03-21 16:31:28 -0700

    api

--- pages/about.js
+++ pages/about.js
@@ -36,7 +36,7 @@ export default function About() {
   const onSubmitForm = async (value) => {
     let config = {
       method: "post",
-      url: "http://localhost:3000/api/contact",
+      url: "./api/contact.js",
       headers: { "Content-type": "application/json" },
       data: value,
     };
