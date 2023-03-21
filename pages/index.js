b8218dadce05865c78cdae76cd5c4f6fe95316a5
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2023-03-21 16:16:57 -0700

    favicon

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import About from "./about";
-
+import Head from "next/head";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Taiwan Matters</title>
+      </Head>
       <About />
     </>
   );
