4c402b8ba793ab3537de98109e836d5b4cc235e8
shihchinghuang <82016503+shihchinghuang@users.noreply.github.com>
2023-10-19 16:23:00 -0700

    the new york times api

--- pages/feed.js
+++ pages/feed.js
@@ -1,15 +1,50 @@
 import Toolbar from "../components/toolbar";
 import styles from "../styles/Feed.module.css";
 import Head from "next/head";
-import Image from "next/image";
 
-export default function Feed({ articles }) {
+export default function Feed({ articles, data }) {
+  let headlineImg = data[0].multimedia.filter(
+    (item) => item.subtype === "facebookJumbo"
+  );
   return (
     <>
       <Head>
         <title>Taiwan Matters</title>
       </Head>
       <Toolbar />
+
+      <div className={styles.headline}>
+        <p className={styles.headlineDesc}>🌟 Recently, this matters most: </p>
+        <a
+          className={styles.headlineLink}
+          href={data[0].web_url}
+          target="_blank"
+        >
+          <div className={styles.headlineStory}>
+            <div>
+              <img
+                className={styles.headlineImg}
+                src={`https://www.nytimes.com/${headlineImg[0].url}`}
+                alt="photo"
+              />
+            </div>
+            <div>
+              <p className={styles.headlineTitle}>{data[0].headline.main}</p>
+              <p className={styles.desc}>
+                {data[0].abstract}
+                <br />
+                {data[0].lead_paragraph}
+              </p>
+              <div className={styles.headlineSrc}>
+                <p>{data[0].pub_date.split("T").join(" ").slice(0, 16)}</p>
+                <p>{data[0].source}</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <img src="/down-arrow.png" className={styles.downArrow} />
       <div className={styles.main}>
         {articles.map((article, index) => {
           return (
@@ -44,7 +79,7 @@ export const getServerSideProps = async () => {
   let day = date.getDate() - 2;
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
-  const apiResponse = await fetch(
+  const apiResponseNews = await fetch(
     `https://newsapi.org/v2/everything?q=taiwan&from=${year}-${month}-${day}&language=en&sortBy=popularity`,
     {
       headers: {
@@ -52,12 +87,22 @@ export const getServerSideProps = async () => {
       },
     }
   );
-  const apiJson = await apiResponse.json();
-  let { articles } = apiJson;
+  const apiJsonNews = await apiResponseNews.json();
+  let { articles } = apiJsonNews;
+  const apiResponseNYT = await fetch(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=taiwan&page=1&sort=newest&api-key=${process.env.NEXT_PUBLIC_NEWSFROMNYT_KEY}`
+  );
+  let apiJsonNYT = await apiResponseNYT.json();
+  let data = apiJsonNYT.response.docs;
+  data = data.filter(
+    (item) =>
+      item.abstract.includes("taiwan") || item.abstract.includes("Taiwan")
+  );
   articles = articles.filter(
     (article) => article.urlToImage && article.title.includes("Taiwan")
   );
+
   return {
-    props: { articles },
+    props: { articles, data },
   };
 };
